634c25a0fce77882d61d2dbd22401bf742538543
hyunwook <c004245@naver.com>
2018-10-30 10:20:51 +0900

    카드 리더기 (USB) 장착 시 추가 AsyncTask, 카드 리더 시 리스너.

--- app/src/main/java/com/example/hyunwook/testreaderacr122u/MainActivity.java
+++ app/src/main/java/com/example/hyunwook/testreaderacr122u/MainActivity.java
@@ -1,16 +1,165 @@
 package com.example.hyunwook.testreaderacr122u;
 
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.acs.smartcard.Reader;
+import com.acs.smartcard.ReaderException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mListButton; //연결된 장치 리스트 확인
+    private Button mOpenButton; //장치 오픈
+
+    private UsbManager mManager;
+
+    private Reader mReader;
+    private PendingIntent mPermissionIntent;
+    private static final String[] stateStrings = { "Unknown", "Absent",
+            "Present", "Swallowed", "Powered", "Negotiable", "Specific" };
+
+    static final String TAG = MainActivity.class.getSimpleName();
+    private static final String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mManager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
+        //Initialize reader
+        mReader = new Reader(mManager);
+        mReader.setOnStateChangeListener(new Reader.OnStateChangeListener() {
+            @Override
+            public void onStateChange(int slotNum, int prevState, int currState) {
+
+                if (prevState < Reader.CARD_UNKNOWN
+                       || prevState > Reader.CARD_SPECIFIC) {
+                    prevState = Reader.CARD_UNKNOWN;
+                }
+
+                if (currState < Reader.CARD_UNKNOWN
+                        || currState > Reader.CARD_SPECIFIC) {
+                    currState = Reader.CARD_UNKNOWN;
+                }
+
+                // Create output string
+                final String outputString = "Slot " + slotNum + ": "
+                        + stateStrings[prevState] + " -> "
+                        + stateStrings[currState];
+
+                if (currState == Reader.CARD_PRESENT) {
+                    Log.d(TAG, "Ready to read...");
+                        final byte[] command = {(byte) 0xFF, (byte) 0xCA, (byte) 0x00, (byte) 0x00, (byte) 0x00};
+
+                        final byte[] response = new byte[256];
+                        try {
+                            int byteCount = mReader.control(slotNum, Reader.IOCTL_CCID_ESCAPE,
+                                    command, command.length, response, response.length);
+
+                            //get UID
+                            StringBuffer uid = new StringBuffer();
+
+                            for (int i = 0; i < (byteCount -2); i++) {
+                                uid.append(String.format("%02X", response[i]));
+
+                                if (i < byteCount -3) {
+
+                                }
+                                try {
+                                    Log.d(TAG, "Data --->" + Long.parseLong(uid.toString(), 16));
+                                } catch (NumberFormatException e) {
+                                    Looper.prepare();
+                                    Toast.makeText(getApplicationContext(), "인식 실패, 다시 찍어주세요.", Toast.LENGTH_LONG);
+                                    Looper.loop();
+                                }
+                            }
+
+                         } catch (ReaderException e) {
+                            e.printStackTrace();
+                        }
+                }
+
+            }
+        });
+
+        //Register receiver for USB permission
+        mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(mReceiver, filter);
+
+
+
+
+    }
+
+    //USB Receiver
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+
+                        if (device != null) {
+                            //Open Reader
+                            new OpenTask().execute(device);
+                        }
+
+                    }
+                }
+            }
+        }
+    };
+
+    //Reader Device OpenTask
+    private class OpenTask extends AsyncTask<UsbDevice, Void, Exception> {
+
+        @Override
+        protected Exception doInBackground(UsbDevice... params) {
+            Exception result = null;
+
+            try {
+                mReader.open(params[0]);
+            } catch (Exception e) {
+                result = e;
+            }
+
+            return result;
+        }
+        @Override
+        protected void onPostExecute(Exception result) {
+            if (result != null) {
+
+            } else {
+                Log.d(TAG, "Reader Name -->" + mReader.getReaderName());
+
+                int numSlots = mReader.getNumSlots();
+                Log.d(TAG, "Number of slots: " + numSlots);
+
+                //Add Slot items;
+//                mslo
+                
+            }
+        }
     }
 }
